7393785297f56dbbfd6253485aeaac76c2c8191c
Gabriela Dodson <dodsongabi@gmail.com>
2024-09-17 17:41:55 -0400

    Initialize inventory with product objects

---
+++ cc_5.js
@@ -0,0 +1,7 @@
+//Create an Inventory Array of Product Objects
+const inventory = [
+    {name:'Latte', price:'7.99', quantity:'80' },
+    {name:'Pastry', price:'5.99', quantity:'50' },
+    {name:'Tea', price:'6.99', quantity:'60' },
+    {name:'Mug', price:'15.99', quantity:'25' }
+];
